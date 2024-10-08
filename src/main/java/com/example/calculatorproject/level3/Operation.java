9602ab0a42f52f91ef7435b727622d9391e3938f
Fangsangik <hwangsangik@gmail.com>
2024-10-08 19:09:02 +0900

    refactor = "Double이 왔을경우 Double type으로 받기 위해 double type으로 받게 했고, Integer, Long, Float도 마찮가지로 설정"

--- src/main/java/com/example/calculatorproject/level3/Operation.java
+++ src/main/java/com/example/calculatorproject/level3/Operation.java
@@ -1,36 +1,79 @@
 package com.example.calculatorproject.level3;
 
 
+//코드 중복이 많은데 해결 방벙?
 public enum Operation {
     ADD("+") {
-        public double apply(double a, double b) {
-            return a + b;
+        @Override
+        public <T extends Number> T apply(T a, T b) {
+            if (a instanceof Double || b instanceof Double) {
+                return (T) Double.valueOf(a.doubleValue() + b.doubleValue());
+            } else if (a instanceof Integer || b instanceof Integer) {
+                return (T) Integer.valueOf(a.intValue() + b.intValue());
+            } else if (a instanceof Long || b instanceof Long) {
+                return (T) Long.valueOf(a.longValue() + b.longValue());
+            } else if (a instanceof Float || b instanceof Float) {
+                return (T) Float.valueOf(a.floatValue() + b.floatValue());
+            } else {
+                throw new IllegalArgumentException("지원되지 않는 타입입니다.");
+            }
         }
     },
 
     SUBTRACT("-") {
-        public double apply(double a, double b) {
-            return a - b;
+        @Override
+        public <T extends Number> T apply(T a, T b) {
+            if (a instanceof Double || b instanceof Double) {
+                return (T) Double.valueOf(a.doubleValue() - b.doubleValue());
+            } else if (a instanceof Integer || b instanceof Integer) {
+                return (T) Integer.valueOf(a.intValue() - b.intValue());
+            } else if (a instanceof Long || b instanceof Long) {
+                return (T) Long.valueOf(a.longValue() - b.longValue());
+            } else if (a instanceof Float || b instanceof Float) {
+                return (T) Float.valueOf(a.floatValue() - b.floatValue());
+            } else {
+                throw new IllegalArgumentException("지원되지 않는 타입입니다.");
+            }
         }
     },
-
     MULTIPLY("*") {
-        public double apply(double a, double b) {
-            return a * b;
+        @Override
+        public <T extends Number> T apply(T a, T b) {
+            if (a instanceof Double || b instanceof Double) {
+                return (T) Double.valueOf(a.doubleValue() * b.doubleValue());
+            } else if (a instanceof Integer || b instanceof Integer) {
+                return (T) Integer.valueOf(a.intValue() * b.intValue());
+            } else if (a instanceof Long || b instanceof Long) {
+                return (T) Long.valueOf(a.longValue() * b.longValue());
+            } else if (a instanceof Float || b instanceof Float) {
+                return (T) Float.valueOf(a.floatValue() * b.floatValue());
+            } else {
+                throw new IllegalArgumentException("지원되지 않는 타입입니다.");
+            }
         }
     },
-
     DIVIDE("/") {
-        public double apply(double a, double b) {
-            if (b == 0) {
+        @Override
+        public <T extends Number> T apply(T a, T b) {
+            if (b.doubleValue() == 0) {
                 throw new ArithmeticException("0으로 나눌 수 없습니다.");
             }
-            return a / b;
+
+            if (a instanceof Double || b instanceof Double) {
+                return (T) Double.valueOf(a.doubleValue() / b.doubleValue());
+            } else if (a instanceof Integer || b instanceof Integer) {
+                return (T) Integer.valueOf(a.intValue() / b.intValue());
+            } else if (a instanceof Long || b instanceof Long) {
+                return (T) Long.valueOf(a.longValue() / b.longValue());
+            } else if ( a instanceof Float || b instanceof Float) {
+                return (T) Float.valueOf(a.floatValue() / b.floatValue());
+            } else {
+                throw new IllegalArgumentException("지원되지 않는 타입입니다.");
+            }
         }
     };
 
-
-    private String symbol;
+    private final String symbol;
 
     Operation(String symbol) {
         this.symbol = symbol;
@@ -40,5 +83,5 @@ public enum Operation {
         return symbol;
     }
 
-    public abstract double apply(double a, double b);
+    public abstract <T extends Number> T apply(T a, T b);
 }
