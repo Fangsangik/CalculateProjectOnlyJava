8c6d3ac4b5da3498a95f069a8e93b2b92d2bfc18
Fangsangik <hwangsangik@gmail.com>
2024-10-08 19:06:27 +0900

    refactor = "다양한 숫자 타입을 받기 위해 제네릭 T로 변경했다."

--- src/main/java/com/example/calculatorproject/level3/ArithmeticCalculator.java
+++ src/main/java/com/example/calculatorproject/level3/ArithmeticCalculator.java
@@ -10,13 +10,10 @@ public class ArithmeticCalculator <T extends Number & Comparable<T>> {
 //제네릭이 Number type에 맞게 결과 값 가져오기
 public class ArithmeticCalculator<T extends Number> {
 
-    public double calculate(T a, T b, Operation operation) {
-        double num1 = a.doubleValue();
-        double num2 = b.doubleValue();
-        double result = operation.apply(num1, num2);
-        rsts.add(result);
+    private final List<T> resultList = new ArrayList<>();
 
-        return result;
+    public T calculate(T a, T b, Operation operatorType) {
+        return operatorType.apply(a, b);
     }
 
     public List<Double> getBiggerRst(T val) {
