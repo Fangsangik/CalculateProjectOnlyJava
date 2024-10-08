20e6a8faf41d404029ba3b9b88bfe20eae99747a
Fangsangik <hwangsangik@gmail.com>
2024-10-08 19:07:52 +0900

    refactor = "변수 이름 변경, a -> num1 , b -> num2"

--- src/main/java/com/example/calculatorproject/level1_with_class/Operation.java
+++ src/main/java/com/example/calculatorproject/level1_with_class/Operation.java
@@ -1,8 +1,8 @@
 package com.example.calculatorproject.level1_with_class;
 
 public interface Operation {
-    int add(int a, int b);
-    int subtract(int a, int b);
-    int multiply(int a, int b);
-    int divide(int a, int b);
+    int add(int num1, int num2);
+    int subtract(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
 }
