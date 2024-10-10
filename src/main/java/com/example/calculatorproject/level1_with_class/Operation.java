45e0ed62ff7d7ce6ac2d79edbf2642ee65f31934
Fangsangik <hwangsangik@gmail.com>
2024-10-10 14:14:00 +0900

    refactor = "클래스 사용해 계산기 기능 구현하기"

---
+++ src/main/java/com/example/calculatorproject/level1_with_class/Operation.java
@@ -0,0 +1,8 @@
+package com.example.calculatorproject.level1_with_class;
+
+public interface Operation {
+    int add(int num1, int num2);
+    int subtract(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
+}
