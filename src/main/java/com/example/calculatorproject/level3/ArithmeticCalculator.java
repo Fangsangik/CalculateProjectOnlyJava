7d30ae2fb28fe2e2d52ee93cb008f605a1f8aed3
Fangsangik <hwangsangik@gmail.com>
2024-10-14 19:06:25 +0900

    refactor = "Operation logic을 보면, Double이 Integer 값을 포함함으로 Integer type과 Double 타입을 연산 기능 부분에서 실행 할 필요가 없어 Double로 입력값을 통일했다."

--- src/main/java/com/example/calculatorproject/level3/ArithmeticCalculator.java
+++ src/main/java/com/example/calculatorproject/level3/ArithmeticCalculator.java
@@ -7,26 +7,27 @@ import java.util.stream.Collectors;
 //Number -> int, double, long등 부모형 타입
 
 //제네릭이 Number type에 맞게 결과 값 가져오기
-public class ArithmeticCalculator<T extends Number> {
+public class ArithmeticCalculator<T extends Double> {
 
-    private final List<T> resultList = new ArrayList<>();
+    private final List<Double> resultList = new ArrayList<>();
 
-    public Number calculate(T a, T b, Operation operatorType) {
+    public Double calculate(double a, double b, Operation operatorType) {
         return operatorType.apply(a, b);
     }
 
     // List에서 입력된 값보다 큰 값을 필터링하는 메서드
-    public List<Number> getResultsGreaterThan(List<Number> resultList, Number compareVal) {
-        List<Number> filteredResults = new ArrayList<>();
-        for (Number result : resultList) {
-            if (result.doubleValue() > compareVal.doubleValue()) {
+    public List<Double> getResultsGreaterThan(List<Double> resultList, Double compareVal) {
+        List<Double> filteredResults = new ArrayList<>();
+        for (Double result : resultList) {
+            if (result > compareVal) {
+                // 소수점 이하가 없는 경우, 소수점 없이 출력
                 filteredResults.add(result);
             }
         }
         return filteredResults;
     }
 
-    public List<T> getResultList() {
+    public List<Double> getResultList() {
         return resultList;
     }
 }
