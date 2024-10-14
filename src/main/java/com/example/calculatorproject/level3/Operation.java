5d1f131d9d2f389bf6ad8dacb572b25f01f8c986
Fangsangik <hwangsangik@gmail.com>
2024-10-14 19:06:34 +0900

    refactor = "Operation logic을 보면, Double이 Integer 값을 포함함으로 Integer type과 Double 타입을 연산 기능 부분에서 실행 할 필요가 없어 Double로 입력값을 통일했다."

--- src/main/java/com/example/calculatorproject/level3/Operation.java
+++ src/main/java/com/example/calculatorproject/level3/Operation.java
@@ -2,48 +2,40 @@ package com.example.calculatorproject.level3;
 
 
 //코드 중복이 많은데 해결 방벙?
+//목적, 전치리, 후치리 -> 편하게 개발 할 수 있는 방향
+//내부적으로 하나만 결정해서, 보여주는 값은 인티저로
+//if 문으로 가냐, 파라미터로 가냐.
 public enum Operation {
+
     ADD("+") {
         @Override
-        public Number apply(Number a, Number b) {
-            if (a instanceof Integer && b instanceof Integer) {
-                return a.intValue() + b.intValue();
-            } else {
-                return a.doubleValue() + b.doubleValue();
-            }
+        public Double apply(double a, double b) {
+            return a + b;
         }
     },
+
     SUBTRACT("-") {
         @Override
-        public Number apply(Number a, Number b) {
-            if (a instanceof Integer && b instanceof Integer) {
-                return a.intValue() - b.intValue();
-            } else {
-                return a.doubleValue() - b.doubleValue();
-            }
+        public Double apply(double a, double b) {
+            return a - b;
         }
     },
     MULTIPLY("*") {
         @Override
-        public Number apply(Number a, Number b) {
-            if (a instanceof Integer && b instanceof Integer) {
-                return a.intValue() * b.intValue();
-            } else {
-                return a.doubleValue() * b.doubleValue();
-            }
+        public Double apply(double a, double b) {
+
+            return a * b;
+
         }
     },
+
     DIVIDE("/") {
         @Override
-        public Number apply(Number a, Number b) {
-            if (b.doubleValue() == 0) {
+        public Double apply(double a, double b) {
+            if (b == 0) {
                 throw new ArithmeticException("0으로 나눌 수 없습니다.");
             }
-            if (a instanceof Integer && b instanceof Integer) {
-                return a.intValue() / b.intValue();
-            } else {
-                return a.doubleValue() / b.doubleValue();
-            }
+            return a / b;
         }
     };
 
@@ -57,5 +49,5 @@ public enum Operation {
         return symbol;
     }
 
-    public abstract Number apply(Number a, Number b);
+    public abstract Double apply(double a, double b);
 }
