a02706a281d8c6ddf32d4c21d804849878fc8773
Fangsangik <hwangsangik@gmail.com>
2024-10-16 09:16:14 +0900

    refactor = "기능 설명"

--- src/main/java/com/example/calculatorproject/level3/Operation.java
+++ src/main/java/com/example/calculatorproject/level3/Operation.java
@@ -5,6 +5,8 @@ package com.example.calculatorproject.level3;
 //목적, 전치리, 후치리 -> 편하게 개발 할 수 있는 방향
 //내부적으로 하나만 결정해서, 보여주는 값은 인티저로
 //if 문으로 가냐, 파라미터로 가냐.
+
+//operation Enum 타입에 정보를 관리, 사칙 연산 기능 수행
 public enum Operation {
 
     ADD("+") {
