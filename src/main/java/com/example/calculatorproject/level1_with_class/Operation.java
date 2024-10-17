8560fba5dc3f4f9e49438d65e997a2cf914ed2c2
Fangsangik <hwangsangik@gmail.com>
2024-10-17 10:08:45 +0900

    refactor = "please check branch"

--- src/main/java/com/example/calculatorproject/level1_with_class/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.example.calculatorproject.level1_with_class;
-
-//인터페이스를 사용해 단순 기능적 부분만 할 수 있도록
-public interface Operation {
-    int add(int num1, int num2);
-    int subtract(int num1, int num2);
-    int multiply(int num1, int num2);
-    int divide(int num1, int num2);
-}
